0d80a1455f9dda650332e95d9da950b14c577ffb
Marek Kotowski <32248131+marekkotowski87@users.noreply.github.com>
2025-06-27 15:07:27 +0200

    Refactor bottom navigation into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
 import {
   AppBar,
   Toolbar,
   Typography,
   Button,
   CssBaseline,
-  BottomNavigation,
-  BottomNavigationAction,
-  Paper,
 } from '@mui/material'
-import HomeIcon from '@mui/icons-material/Home'
-import ExploreIcon from '@mui/icons-material/TravelExplore'
-import SettingsIcon from '@mui/icons-material/Settings'
+import BottomNav from './components/BottomNav'
 
 function App() {
-  const [value, setValue] = useState(0)
 
   return (
     <>
@@ -27,17 +20,7 @@ function App() {
           <Button color="inherit">Log in</Button>
         </Toolbar>
       </AppBar>
-      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(event, newValue) => setValue(newValue)}
-        >
-          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-          <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
-          <BottomNavigationAction label="Settings" icon={<SettingsIcon />} />
-        </BottomNavigation>
-      </Paper>
+      <BottomNav />
     </>
   )
 }
